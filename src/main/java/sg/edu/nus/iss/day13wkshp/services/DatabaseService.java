fd9eef670c0bd466c99345b80eb6cb360ed50f9d
daryl-goh <goh.daryl@gmail.com>
2022-07-20 16:01:33 +0800

    final commit

--- src/main/java/sg/edu/nus/iss/day13wkshp/services/DatabaseService.java
+++ src/main/java/sg/edu/nus/iss/day13wkshp/services/DatabaseService.java
@@ -2,6 +2,9 @@ package sg.edu.nus.iss.day13wkshp.services;
 
 import java.io.*;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 import org.springframework.stereotype.Service;
@@ -48,15 +51,23 @@ public class DatabaseService {
 	public Contact read(String fileId) {
 
         try {
-
+/* 
             File f = new File(this.dataDir, fileId);
             Scanner myReader = new Scanner(f);
             while (myReader.hasNextLine()) {
                 System.out.println(myReader.nextLine());
             }
-            myReader.close();
+            myReader.close();*/
 
             Contact contact = new Contact();
+
+            Path filePath = new File(this.dataDir, fileId).toPath();
+            Charset charset = Charset.forName("utf-8");
+            List<String> stringVal = Files.readAllLines(filePath, charset);
+
+            contact.setName(stringVal.get(1));
+            contact.setEmail(stringVal.get(2));
+            contact.setPhone(stringVal.get(3));
             return contact;
 
         } catch (IOException ex) {
